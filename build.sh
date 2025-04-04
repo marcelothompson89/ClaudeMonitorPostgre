65987f73162d1ef72f03a4e8ec11b0097f251f1a
Marcelo Thompson <marcelo.thompson@altromondo.com.ar>
2025-04-04 12:20:40 -0300

    Actualizar build para error chromium

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-playwright install chromium-headless-shell
+playwright install chromium
 # Ejecuta collectstatic primero (y usa --clear)
 python manage.py collectstatic --no-input --clear
 python manage.py migrate
